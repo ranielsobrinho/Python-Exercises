9cb25cc41e5dec1c53e1b73828968cb3a8e331f9
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2020-03-30 22:27:39 -0300

    Um exercício básico.
    Um código básico que recebe a idade do usuário e diz se ele é maior de idade ou menor de idade,

---
+++ exercício1.py
@@ -0,0 +1,7 @@
+#Verificação de maior idade
+maior_idade = 18
+idade_usuario = int(input("Qual é a sua idade"))
+if idade_usuario > maior_idade:
+    print("Você é maior de idade")
+else:
+    print("Você não é maior de idade")
