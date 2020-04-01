2028a59cd24727769010431f50193f7eaff35d0c
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2020-04-01 13:44:58 -0300

    Nova atualização no código: Foi implementado um sistema de loop que so é encerrado quando pressionado 'q' ou 'Q'.

--- Calculadora.py
+++ Calculadora.py
@@ -1,22 +1,27 @@
-valor1 = float(input("Digite o primeiro valor:"))
-operador = input("Digite o operador matemático:")
-valor2 = float(input("Digite o segundo valor:"))
-#  -- soma --
-if operador == "+":
-    resultado = valor1 + valor2
-#  -- subtração --
-elif operador == "-":
-    resultado = valor1 - valor2
-#  -- multiplicação --
-elif operador == "*":
-    resultado = valor1 * valor2
-#  -- divisão --
-elif operador == "/":
-    resultado = valor1 / valor2
-else:
-    resultado = "Operador não reconhecido"
-
-
-print(resultado)
-
+#estudando estrutura de repetição
+while True:
+    operador = input("Digite '+','-','*' ou '/' para fazer a operação matemática:")
+    if operador == 'q' or operador == 'Q':
+        break
+    elif operador == '+' or operador == '-' or operador == '*' or operador == '/':
+        valor1 = float(input("Digite o primeiro valor :"))
+        valor2 = float(input("Digite o segundo valor: "))
+    else:
+        print("Operador não válido, por favor tente novamente.")
+    #  -- soma --
+    if operador == "+":
+        resultado = valor1 + valor2
+        print(resultado)
+    #  -- subtração --
+    elif operador == "-":
+        resultado = valor1 - valor2
+        print(resultado)
+    #  -- multiplicação --
+    elif operador == "*":
+        resultado = valor1 * valor2
+        print(resultado)
+    #  -- divisão --
+    elif operador == "/":
+        resultado = valor1 / valor2
+        print(resultado)
 
